fda245aeea9a85e3ceb70807d4282310cc105869
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-12-14 19:57:07 -0300

    create pages user

---
+++ src/AleffGroup.WebMvc/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿using AleffGroup.WebMvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AleffGroup.WebMvc.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserViewModel UserViewModelMoc = new UserViewModel() {UserId = 1, Name="TST NAME", Login="LOGIN", Senha="Snh", IsAdmin=false};
+        public ActionResult Index()
+        {
+            //return View(Enumerable.Empty<UserViewModel>());
+            return View(new List<UserViewModel>() { UserViewModelMoc });
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View(UserViewModelMoc);
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+       
+        public ActionResult Edit(int id)
+        {
+            return View(UserViewModelMoc);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(UserViewModelMoc);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ src/AleffGroup.WebMvc/ViewModels/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AleffGroup.WebMvc.ViewModels
+{
+    public class UserViewModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [DisplayName("Nome do Usuário")]
+        [Required(ErrorMessage = "Preencha o campo Nome")]
+        [MaxLength(100, ErrorMessage = "Máximo {0} caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Login")]
+        [MaxLength(50, ErrorMessage = "Máximo {0} caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo E-mail")]
+        [MaxLength(10, ErrorMessage = "Máximo {0} caracteres")]
+        [DisplayName("Senha")]
+        public string Senha { get; set; }
+
+        [DisplayName("Admin")]
+        public bool IsAdmin { get; set; }
+    }
+}
