d188624827ae0b8ebab2f8be239914979cf134e1
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-12-14 09:36:08 -0300

    create entities

---
+++ src/AleffGroup.Domain/Entities/LogAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AleffGroup.Domain.Entities
+{
+    public class LogAccess
+    {
+        public int LogAcessoId { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataHoraAcesso { get; set; }
+        public string EnderecoIp { get; set; }
+
+    }
+}
---
+++ src/AleffGroup.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AleffGroup.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
