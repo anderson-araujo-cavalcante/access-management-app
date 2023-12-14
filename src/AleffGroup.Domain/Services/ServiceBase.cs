cbaf38d0301a978a03f9482ac6b5d1b87c7a6a29
Anderson Araujo Cavalcante <133878123+anderson-araujo-cavalcante@users.noreply.github.com>
2023-12-14 20:32:25 -0300

    create user service

---
+++ src/AleffGroup.Domain/Services/ServiceBase.cs
@@ -0,0 +1,52 @@
+﻿using AleffGroup.Domain.Interfaces.Repositories;
+using AleffGroup.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+
+namespace AleffGroup.Domain.Services
+{
+    public class ServiceBase<TEntity, TKey> : IDisposable, IServiceBase<TEntity, TKey> where TEntity : class
+    {
+        private readonly IRepositoryBase<TEntity, TKey> _repository;
+
+        public ServiceBase(IRepositoryBase<TEntity, TKey> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(TEntity entity)
+        {
+            _repository.Add(entity);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public TEntity GetById(TKey id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _repository.Remove(entity);
+        }
+
+        public void Remove(TKey id)
+        {
+            _repository.Remove(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _repository.Update(entity);
+        }
+
+        public void Dispose()
+        {
+            _repository.Dispose();
+        }
+    }
+}
---
+++ src/AleffGroup.Domain/Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using AleffGroup.Domain.Interfaces.Repositories;
+
+namespace AleffGroup.Domain.Services
+{
+    public class UserService : ServiceBase<Entities.User, int>, Interfaces.Services.IUserService
+    {
+        //private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository clienteRepository)
+            : base(clienteRepository)
+        {
+            //_userRepository = clienteRepository;
+        }
+    }
+}
